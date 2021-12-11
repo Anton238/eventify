6050cb47b2ae42139f3b6d42515ee70129818f56
Anton238 <antonsorokin238@gmail.com>
2021-12-11 18:28:34 +0300

    [event-1]

---
+++ event/src/main/java/com/eventify/events/EventsApplication.java
@@ -0,0 +1,16 @@
+package com.eventify.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EventsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EventsZApplication.class, args);
+	}
+
+}
---
+++ event/src/test/java/com/eventify/event/EventApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eventify.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EventApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
