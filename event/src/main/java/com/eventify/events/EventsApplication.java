cfda57f43e40201b6ce1aa2a8d67ef957bf6a6a0
Anton238 <antonsorokin238@gmail.com>
2021-12-12 10:58:04 +0300

    [event-1]

--- event/src/main/java/com/eventify/events/EventsApplication.java
+++ event/src/main/java/com/eventify/events/EventsApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class EventsApplication
-{
+public class EventsApplication {
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(EventsZApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventsApplication.class, args);
+    }
 
 }
